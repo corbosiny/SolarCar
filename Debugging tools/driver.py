28b5a3ffceadf5dbd02829be4d7da0efa2c3a0d6
danny-vo <dannyphvo@gmail.com>
2018-10-15 10:39:14 -0500

    Fixing threading stuff

--- Debugging tools/driver.py
+++ Debugging tools/driver.py
@@ -2,16 +2,19 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication
+from threading import Thread
 
 from gui.serialMonitorViewer import SerialMonitorInterface
 from serialMonitor.serialMonitor import SerialMonitor
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
-  serialMonitorInterface = SerialMonitorInterface()
+  smi = SerialMonitorInterface()
 
   comPort = "/dev/ttyACM0"
   serialMonitor = SerialMonitor(comPort)
-  serialMonitorInterface.assignMonitor(serialMonitor)
+  smi.assignMonitor(serialMonitor)
   
+  # Thread(target=smi.periodicPowerPoll).start()
+  # smi.monitorThreads["VoltageIn\n"] = None
   sys.exit(app.exec())
